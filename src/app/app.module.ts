c5b1e2c092388af7b29cb33939de38e84ca75b27
= <nabcogroup@gmail.com>
2017-10-19 12:18:21 +0300

    Initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
+import { PropertyService } from './data-services/property-services/property.service';
+
+
+import { PropertyObjectModule } from './modules/property-object/property-object.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+
+
+import { DashboardComponent } from './sub-module/dashboard/dashboard.component';
+
+import { AppContentComponent } from './app-content/app-content.component';
+import { AppSidebarComponent } from './app-sidebar/app-sidebar.component';
+import { AppHeaderComponent } from './app-header/app-header.component';
+
+
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppSidebarComponent,
+    AppContentComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    PropertyObjectModule
   ],
-  providers: [],
+  providers: [
+    PropertyService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
